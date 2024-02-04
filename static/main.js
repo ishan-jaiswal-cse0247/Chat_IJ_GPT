85535c3fc83c11567b1055a2e4075d8361b4fcfc
Ishan Jaiswal <69758646+ishan-jaiswal-cse0247@users.noreply.github.com>
2024-02-04 14:01:23 +0530

    Add files via upload

--- static/main.js
+++ static/main.js
@@ -1,1 +1,43 @@
+$(document).ready(function () {
+  $('#sendButton').on('click', function () {
+    getBotResponse();
+  });
 
+  $('#textInput').keypress(function (e) {
+    if (e.which == 13) {
+      getBotResponse();
+    }
+  });
+
+  function scrollToBottom() {
+    var chatbox = document.getElementById('chatbox');
+    chatbox.scrollTop = chatbox.scrollHeight;
+  }
+
+  function getBotResponse() {
+    var rawText = $('#textInput').val().trim();
+    if (!rawText) return;
+
+    var userHtml =
+      '<div class="message user-message"><span>' + rawText + '</span></div>';
+    $('#textInput').val('');
+    $('#chatbox').append(userHtml);
+
+    scrollToBottom();
+
+    var loadingHtml =
+      '<div class="message bot-message loading"><span class="dot-1"></span><span class="dot-2"></span><span class="dot-3"></span> Loading</div>';
+    $('#chatbox').append(loadingHtml);
+
+    scrollToBottom();
+
+    $.get('/get', { msg: rawText }).done(function (data) {
+      $('.loading').remove();
+      var botHtml =
+        '<div class="message bot-message"><span>' + data + '</span></div>';
+      $('#chatbox').append(botHtml);
+
+      scrollToBottom();
+    });
+  }
+});
